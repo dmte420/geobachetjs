f5a49a583fd81de79ec198aabc9f7142b5d0ee97
dmte420 <dmtecompanygroup@gmail.com>
2026-05-24 18:33:56 -0700

    fix: quito formatjs y plugins babel obsoletos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,3 @@
-import '@formatjs/intl-getcanonicallocales/polyfill';
-import '@formatjs/intl-locale/polyfill';
-import '@formatjs/intl-numberformat/polyfill';
-import '@formatjs/intl-pluralrules/polyfill';
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
