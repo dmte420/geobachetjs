d93a5b85d4144f5a9d57721607500ed38ff915d7
dmte420 <dmtecompanygroup@gmail.com>
2026-05-25 21:43:23 -0700

    fix: sdk hermes + clean install + build-properties

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,8 @@
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+        'react-native-reanimated/plugin',
+    ],
   };
 };
